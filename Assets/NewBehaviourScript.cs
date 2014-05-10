c4d16f54cd6af0524de6487f263bfe37166cffd2
MatthewWalker42 <matthew.j.walker42@gmail.com>
2014-05-10 18:06:56 -0400

    Prototype V1
    Prototype V1 exists and runs in the new scene! Including placeholder
    art, rudimentary collisions, and slow movement!

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		IEnumerator wait;
+		for (int i=0; i<3; i++) {
+			CreateUnit ();
+			wait = WaitSecond ();
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateUnit(){
+		GameObject newFighter = (GameObject) Instantiate (Resources.Load ("Placeholder/PlaceholderPlayerFighter"));
+		newFighter.transform.position = new Vector2(-33, -2);
+		}
+	IEnumerator WaitSecond(){
+		yield return new WaitForSeconds (2);
+		}
+}
---
+++ Assets/PlayerFighterBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFighterBehavior : MonoBehaviour {
+	public float speed = 10.1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	
+	}
+
+	void Move(){
+
+		transform.Translate (Vector2.right * speed * Time.deltaTime);
+		}
+}
