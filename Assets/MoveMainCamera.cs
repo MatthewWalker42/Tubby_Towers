4fef716b7029ecb1c5716829f57767facb0a8074
MatthewWalker42 <matthew.j.walker42@gmail.com>
2014-06-05 10:54:41 -0400

    Camera now pans at the start of the game
    camera automatically pans to show enemy tower

--- Assets/MoveMainCamera.cs
+++ Assets/MoveMainCamera.cs
@@ -9,15 +9,45 @@ public class MoveMainCamera : MonoBehaviour {
 	public float maxY = 30f;
 	public float minX = -50f;
 	public float minY = 6f;
+	protected bool startCool = false;
+	protected bool panStart = true;
+	protected bool resetCam = false;
 
 	// Use this for initialization
 	void Start () {
+		//StartCoroutine(waitASecond());
+		
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if(panStart){
+			if(transform.position.x < maxX){
+				transform.position += new Vector3(20f * Time.deltaTime, 0, 0);
+
+			}
+			else if(transform.position.x >= maxX){
+				if (startCool){
+					panStart = false;
+					resetCam = true;
+
+				}
+				else {
+					StartCoroutine(waitASecond());
+				}
+
+
+			}
+		}
+
+		if(resetCam){
+			transform.position = new Vector3(17, 19, -80);
+			resetCam = false;
+		}
+
+		if(!panStart && !resetCam){
 		if(transform.position.x <= minX){
 			transform.position = new Vector3(minX, transform.position.y, transform.position.z);
 		}
@@ -45,6 +75,26 @@ public class MoveMainCamera : MonoBehaviour {
 		if(Input.GetKey(KeyCode.S)){
 			transform.position -= new Vector3(0, speed *Time.deltaTime, 0);
 		}
+	}
 	
+	}
+
+	IEnumerator PanCamera(){
+		bool cooloff = false;
+		while(transform.position.x < maxX){
+			transform.position += new Vector3(1f * Time.deltaTime, 0, 0);
+		}
+		cooloff = true;
+		yield return new WaitForSeconds(2);
+		cooloff = false;
+		if(cooloff == false){
+		   transform.position = new Vector3(17, 19, -80);
+		}
+
+	}
+
+	IEnumerator waitASecond(){
+		yield return new WaitForSeconds(1);
+		startCool = true;
 	}
 }
