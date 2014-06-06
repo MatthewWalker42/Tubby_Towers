df9fe6b5f7e70f973da6ba14bd0a45cd1c34d9b3
MatthewWalker42 <matthew.j.walker42@gmail.com>
2014-06-06 16:46:35 -0400

    Menus have been added
    Start menu, Help menu, Credits menu have been added.

---
+++ Assets/WardenScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WardenScript : MonoBehaviour {
+	protected GameObject playerT;
+	protected GameObject enemyT;
+
+
+	// Use this for initialization
+	void Start () {
+
+		playerT = GameObject.Find("PlaceholderPlayerTower");
+		enemyT = GameObject.Find("PlaceholderEnemyTower");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(playerT == null){
+			//Debug.Log("You Died");
+			Application.LoadLevel("StartMenu");
+		}
+		if(enemyT == null){
+			Application.LoadLevel("StartMenu");
+		}
+	
+	}
+}
