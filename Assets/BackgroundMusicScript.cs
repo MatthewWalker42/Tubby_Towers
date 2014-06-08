083cabb25089f2fa6e74533f3b1dcbfc1baed3fa
MatthewWalker42 <matthew.j.walker42@gmail.com>
2014-06-08 19:11:16 -0400

    Background Music has been added to the level
    Doo de doo de doo de doooooo

---
+++ Assets/BackgroundMusicScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicScript : MonoBehaviour {
+	public AudioClip otherClip;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!audio.isPlaying){
+			audio.clip = otherClip;
+			audio.Play();
+		}
+
+	
+	}
+}
